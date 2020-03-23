2742959260e092759175333e879e46a195d67d87
Mohan-Zhang-u <morgan.zhang@mail.utoronto.ca>
2020-03-22 21:29:54 -0400

    added gfootball env

--- src/envs/__init__.py
+++ src/envs/__init__.py
@@ -2,6 +2,7 @@ from functools import partial
 #from smac.env import MultiAgentEnv, StarCraft2Env
 from .multiagentenv import MultiAgentEnv
 from .starcraft2 import StarCraft2Env
+from .gfootball import GoogleFootballEnv
 
 import sys
 import os
@@ -11,6 +12,7 @@ def env_fn(env, **kwargs) -> MultiAgentEnv:
 
 REGISTRY = {}
 REGISTRY["sc2"] = partial(env_fn, env=StarCraft2Env)
+REGISTRY["gf"] = partial(env_fn, env=GoogleFootballEnv)
 
 if sys.platform == "linux":
     os.environ.setdefault("SC2PATH",
---
+++ src/envs/gfootball/__init__.py
@@ -0,0 +1,1 @@
+from .gfootball import GF as GoogleFootballEnv
